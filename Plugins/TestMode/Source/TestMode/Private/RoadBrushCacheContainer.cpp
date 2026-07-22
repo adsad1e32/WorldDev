43d696c6f9bd0ca39b0e408d5cea3e22ec224953
adsad1e32 <landyvfx@gmail.com>
2026-07-23 07:46:01 +0800

    Remove legacy TestMode plugin and cleanup rename artifacts

--- Plugins/TestMode/Source/TestMode/Private/RoadBrushCacheContainer.cpp
+++
@@ -1,8 +0,0 @@
-#include "RoadBrushCacheContainer.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(RoadBrushCacheContainer)
-
-URoadBodyBrushCacheContainer::URoadBodyBrushCacheContainer(const FObjectInitializer& ObjectInitializer) :
-	Super(ObjectInitializer)
-{}
-
